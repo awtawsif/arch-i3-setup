9c7d7bf2448f0e6545b2ce436088ddca99d51325
dark-f1are <tawsif7492@gmail.com>
2025-01-15 11:52:54 +0600

    Add copyq to startup applications and package list

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ NC="\033[0m"
 # Consolidated package groups
 PACKAGES=(
     # Core & System
-    htop exa bat upower alacritty brightnessctl nano nano-syntax-highlighting
+    htop exa copyq bat upower alacritty brightnessctl nano nano-syntax-highlighting
     mousepad bash-completion i3-wm i3blocks i3lock fastfetch wget xss-lock
     bluez bluez-utils blueman lxappearance man-db ly network-manager-applet
     # File Management
